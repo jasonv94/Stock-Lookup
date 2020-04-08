a1618533a353171ae8bda33bcd25530183110f79
Jason Varghese <varghe0@unbc.ca>
2020-04-07 20:48:14 -0700

    first commit

---
+++ quotes/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render
+from .models import Stock
+def home(request):
+    import requests
+    import json
+    if request.method=='POST':
+        ticker = request.POST['ticker']
+        api_request=requests.get("https://cloud.iexapis.com/stable/stock/" + ticker + "/quote?token=pk_09501c3fecf4420c9eb91fded7fef739")
+
+
+        try:
+            api=json.loads(api_request.content)
+        except Exception as e:
+            api="Error......."
+        return render(request,'home.html',{'api':api})
+
+    else:
+        return render(request,'home.html',{'ticker':"Enter Proper"})
+
+
+
+def about(request):
+    return render(request,'about.html',{})
+
+def add_stock(request):
+    ticker = Stock.objects.all()
+    return render(request,'add_stock.html',{'ticker':ticker})
